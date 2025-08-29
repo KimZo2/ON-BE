b78795c6b10f4bfc2500e23b7233f8e2de65179d
조원희 <woni0517@naver.com>
2025-08-29 17:28:00 +0900

    refactor : roomService 로직 추가

--- src/main/java/com/KimZo2/Back/service/RoomService.java
+++ src/main/java/com/KimZo2/Back/service/RoomService.java
@@ -3,13 +3,21 @@ package com.KimZo2.Back.service;
 import com.KimZo2.Back.dto.room.RoomCreateDTO;
 import com.KimZo2.Back.entity.Room;
 import com.KimZo2.Back.entity.User;
-import com.KimZo2.Back.repository.DBRepository;
+import com.KimZo2.Back.exception.ws.BadPasswordException;
+import com.KimZo2.Back.exception.ws.RoomFullException;
+import com.KimZo2.Back.exception.ws.RoomNotFoundOrExpiredException;
+import com.KimZo2.Back.repository.RoomRepository;
 import com.KimZo2.Back.repository.UserRepository;
+import com.KimZo2.Back.repository.redis.RedisFunction;
+import com.KimZo2.Back.repository.redis.RedisRoomStore;
+import com.KimZo2.Back.repository.redis.RoomJoinScript;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Duration;
+import java.util.Locale;
 import java.util.UUID;
 
 @Service
@@ -19,7 +27,10 @@ public class RoomService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final DBRepository dbRepository;
+    private final RoomRepository roomRepository;
+    private final RedisRoomStore redisRoomStore;
+    private final RedisFunction redisFunction;
+    private final RoomJoinScript roomJoinScript;
 
     // 방 생성 -> PostGre랑 Redis에 모두 저장 필요
     public UUID createRoom(RoomCreateDTO dto) {
@@ -29,9 +40,16 @@ public class RoomService {
         User creator = userRepository.findByNickname(creatorNickname);
         if(creator == null) throw new IllegalArgumentException("사용자를 찾을 수 없습니다.");
 
+        // roomId 생성
+        UUID roomId = UUID.randomUUID();
+
+        // TTL 시간 설정
+        Duration roomTTL = Duration.ofHours(dto.getRoomTime());
+
         // 활성화 되어 있는 방 중 중복 이름 판단 - Redis
-        if(validateDuplicatedRoomName(dto.getName())) {
-            throw new IllegalArgumentException("이름이 동일한 방이 존재합니다.");
+        String roomName = dto.getName().trim().toLowerCase(Locale.ROOT); // 소문자/대문자/띄어쓰기 방 이름 모두 중복 판단
+        if(!validateDuplicatedRoomName(roomName, roomId, roomTTL)){
+            throw new IllegalStateException("이미 활성화된 같은 이름의 방이 있습니다.");
         }
 
         // PostGre 레코드 생성
@@ -40,7 +58,7 @@ public class RoomService {
                 .maxParticipants(dto.getMaxParticipants())
                 .isPrivate(false)
                 .roomTime(dto.getRoomTime())
-                .currentParticipants(0)
+                .roomType(1) // 룸 타입 일단 defualt 1
                 .creator(creator)
                 .build();
 
@@ -52,50 +70,89 @@ public class RoomService {
         }
 
         // PostGre 커밋
-        dbRepository.save(newRoom);
+        roomRepository.save(newRoom);
 
+        // Reids 런타임 세팅
+        try {
+            long now = System.currentTimeMillis();
+            redisRoomStore.createRoomRuntime(
+                    roomId,
+                    dto.getName(),
+                    dto.isPrivate(),
+                    creator.getId(),
+                    dto.getMaxParticipants(),
+                    dto.getRoomType(),
+                    roomTTL,
+                    now
+            );
+        } catch (RuntimeException e) {
+            // Redis 저장 실패 : 이름 락 해제
+            redisRoomStore.releaseNameLock(roomName);
 
+            // 트랜잭션 실패 반영 로직 둘거임?
+            throw e;
+        }
 
-        // Reids 로직
-
-        // Redis에는 비밀번호 저장해두지 않기
-
-        return null;
+        return roomId;
     }
 
     // 방 이름 중복 조회 -> 현재 방의 상태가 active인 방 중 중복 이름 존재하는지 확인하는 로직
-    public boolean validateDuplicatedRoomName(String roomName) {
-
-        return true;
+    public boolean validateDuplicatedRoomName(String roomName, UUID roomId, Duration roomTTL) {
+        return redisRoomStore.lockRoomName(roomName, roomId.toString(), roomTTL);
     }
 
-    // 방 조회 -> 현재 방의 상태가 active한 방이 있는지 확인
-
-
     // 방 입장
-    public void enterRoom() {
+    public void checkRoom(UUID roomId, String principalName, String roomPW ) {
+        UUID userId = UUID.fromString(principalName);
+        String roomKey = "room:" + roomId;
+
         // 방 ID를 기준으로 방 찾기
-            // 만약 방이 private이라면 비밀번호 요구  -> PostGre에서 검증하기
+        if (!redisFunction.roomHasKey(roomKey)) {
+            throw new RoomNotFoundOrExpiredException("방이 존재하지 않거나 만료되었습니다.");
+        }
+
+        // 만약 방이 private이라면 비밀번호 요구  -> PostGre에서 검증하기
+        if (redisFunction.rommIsPrivate(roomKey)) {
+            checekPassword(roomPW, roomId);
+        }
 
         // 현재 방의 인원이 가득 찼는지 확인
+        long now = System.currentTimeMillis();
+        long r = roomJoinScript.tryJoin(roomId, userId, now);
+        if (r == -1) throw new RoomNotFoundOrExpiredException("방이 존재하지 않거나 만료되었습니다.");
+        if (r == -2) throw new RoomFullException("정원이 가득 찼습니다.");
 
-        // 방 입장 완료 응답
     }
 
 
     // 방 비밀번호 조회
+    public void checekPassword(String roomPW, UUID roomId) {
+        if (roomPW == null || roomPW.isBlank()) {
+            throw new BadPasswordException("비밀번호가 필요합니다.");
+        }
         // 방 ID를 기준으로 방 찾기
+        Room roomEntity = roomRepository.findById(roomId)
+                .orElseThrow(() -> new BadPasswordException("방 정보를 찾을 수 없습니다."));
 
+        // 종료된 방인지 확인
+        if (!roomEntity.isStatus()) {
+            throw new BadPasswordException("이미 종료된 방입니다.");
+        }
+
+        // room 비밀번호 가져오기
+        String hash = roomEntity.getPassword();
         // 비밀번호 확인
-            // 만약 비밀번호가 틀리다면 비밀번호 재요청 응답
+        if (hash == null || !passwordEncoder.matches(roomPW, hash)) {
+            throw new BadPasswordException("비밀번호가 올바르지 않습니다.");
+        }
+    }
 
-        // 현재 방의 인원이 가득 찼는지 확인
+    // 페이지 찾기 로직
+    // 몇 번째 페이지를 보여줄 것인지, 한 페이지에 몇개의 방을 보여줄것인지
 
-        // 방 입장 완료 응답
+    // public 처리가 되어 있는 방중
+    //
 
-
-    // 현재 방의 인원이 가득 찼는지 확인하는 로직
-        // 현재 방의 인원이 가득 찼다면 입장 불가 응답
 
 
 }
