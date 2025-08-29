bdad0d1a203788322be53254f0544a96aa10e0e6
조원희 <woni0517@naver.com>
2025-08-29 17:25:25 +0900

    feat/3 : Room 에러 관련 로직 추가

---
+++ src/main/java/com/KimZo2/Back/exception/ws/GlobalWsExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.KimZo2.Back.exception.ws;
+
+import com.KimZo2.Back.dto.error.ErrorResponse;
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalWsExceptionHandler {
+
+    @MessageExceptionHandler({
+            RoomNotFoundOrExpiredException.class,
+            RoomFullException.class,
+            BadPasswordException.class
+    })
+
+    @SendToUser("/queue/errors")
+    public ErrorResponse handleRoomExceptions(RuntimeException e) {
+
+        String code =
+                (e instanceof RoomFullException) ? "ROOM_FULL" :
+                        (e instanceof BadPasswordException) ? "BAD_PASSWORD" :
+                                "ROOM_NOT_FOUND";
+        return new ErrorResponse(code, e.getMessage());
+    }
+
+    @MessageExceptionHandler(Exception.class)
+    @SendToUser("/queue/errors")
+    public ErrorResponse handleOthers(Exception e) {
+        return new ErrorResponse("ERROR", "알 수 없는 오류가 발생했습니다.");
+    }
+}
+
