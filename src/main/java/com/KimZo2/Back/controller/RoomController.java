e626662752e311e3a472ee82344d3a084b5818e9
조원희 <woni0517@naver.com>
2025-08-30 12:35:17 +0900

    refactor : controller end point 변경

--- src/main/java/com/KimZo2/Back/controller/RoomController.java
+++ src/main/java/com/KimZo2/Back/controller/RoomController.java
@@ -1,22 +1,23 @@
 package com.KimZo2.Back.controller;
 
 import com.KimZo2.Back.dto.room.RoomCreateDTO;
+import com.KimZo2.Back.dto.room.RoomPageResponse;
 import com.KimZo2.Back.service.RoomService;
+import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
+@Validated
 @RequiredArgsConstructor
-@RequestMapping("/api")
+@RequestMapping("/rooms")
 public class RoomController {
 
     private static final Logger log = LoggerFactory.getLogger(RoomController.class);
@@ -24,9 +25,9 @@ public class RoomController {
     private final RoomService roomService;
 
     // 방 생성
-    @PostMapping("/rooms")
+    @PostMapping
     public ResponseEntity<?> createRoom(@RequestBody RoomCreateDTO dto) {
-        log.info("RoomController - GET /rooms  -  실행");
+        log.info("RoomController - POST /rooms  -  실행");
 
         roomService.createRoom(dto);
 
@@ -34,21 +35,14 @@ public class RoomController {
                 .body(Map.of("message", "Room 생성 완료"));
     }
 
-
     // public 방 조회
-//    @GetMapping("/room")
-//    public ResponseEntity<?>
+    @GetMapping
+    public RoomPageResponse listPublicRooms(
+            @RequestParam(defaultValue = "1") @Min(1) int page,
+            @RequestParam(defaultValue = "6") @Min(1) int size
+    ) {
+        log.info("RoomController - POST /rooms  -  실행");
 
-
-
-
-    // private 방 조회
-
-
-
-
-    // 방 입장
-
-
-
+        return roomService.searchRoom(page, size);
+    }
 }
