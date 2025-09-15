6e812acc2ee5171cc589bab27df3dd2dd15178be
조원희 <woni0517@naver.com>
2025-09-15 14:49:19 +0900

    refactor : roomPublic 추가

--- src/main/java/com/KimZo2/Back/util/KeyFactory.java
+++ src/main/java/com/KimZo2/Back/util/KeyFactory.java
@@ -4,16 +4,15 @@ import java.util.UUID;
 
 public final class KeyFactory {
     private KeyFactory() {}
-
     public static String roomMeta(UUID r)          { return "rooms:" + r; }
     public static String roomMembers(UUID r)       { return "rooms:" + r + ":members"; }
     public static String roomPos(UUID r)           { return "rooms:" + r + ":pos"; }
     public static String roomSeen(UUID r)          { return "rooms:" + r + ":seen"; }
-//    public static String roomPosVersion(UUID r)    { return "room:" + r + ":pos:ver"; }
-
     public static String userRoom(UUID userId)   { return "users:" + userId + ":rooms"; }
-
     public static String presence(UUID r, UUID u, String s) { return "presence:" + r + ":" + u + ":" + s; }
     public static String moveRate(UUID r, UUID u) { return "rate:move:" + r + ":" + u; }
     public static String roomHot()  {return "rooms:hot";}
+    public static String roomPublic() {return "rooms:public";}
 }
+
+
