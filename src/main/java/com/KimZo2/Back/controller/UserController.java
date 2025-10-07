ecf920b078e0d4be10da15206755b5cb142723c0
Bellmin <whdals011203@naver.com>
2025-10-07 23:55:56 +0900

    refactor: /member/info Principal 타입 수정

--- src/main/java/com/KimZo2/Back/controller/UserController.java
+++ src/main/java/com/KimZo2/Back/controller/UserController.java
@@ -5,6 +5,7 @@ import com.KimZo2.Back.security.model.CustomUserDetails;
 import com.KimZo2.Back.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +21,8 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/info")
-    public UserInfoResponseDTO getUserInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UUID userId = userDetails.getId();
+    public UserInfoResponseDTO getUserInfo(@AuthenticationPrincipal String id) {
+        UUID userId = UUID.fromString(id);
         return userService.getUserId(userId);
     }
 }
