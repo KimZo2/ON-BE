05bb6e9eaf1730f88414aaa67e80cb56faf1835c
조원희 <woni0517@naver.com>
2025-10-07 23:33:09 +0900

    feat: 특정 방의 유저 퇴장 로직

---
+++ src/main/java/com/KimZo2/Back/repository/redis/RoomCleanUpRepositoryImpl.java
@@ -0,0 +1,36 @@
+package com.KimZo2.Back.repository.redis;
+
+// RoomCleanupRepositoryImpl.java
+
+import com.KimZo2.Back.util.KeyFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@RequiredArgsConstructor
+public class RoomCleanUpRepositoryImpl implements RoomCleanUpRepository {
+
+    private final StringRedisTemplate redisTemplate;
+    private final DefaultRedisScript<Long> cleanupUserLua;
+
+    @Override
+    public Long cleanupExpiredUser(UUID roomId, String userId) {
+        // Lua 스크립트에 전달할 KEYS 리스트
+        List<String> keys = List.of(
+                KeyFactory.roomMembers(roomId),         // KEYS[1]
+                KeyFactory.roomPos(roomId),             // KEYS[2]
+                KeyFactory.roomMeta(roomId),            // KEYS[3]
+                KeyFactory.roomSeen(roomId),            // KEYS[4]
+                KeyFactory.userRoom(UUID.fromString(userId)) // KEYS[5]
+//                KeyFactory.moveRate(roomId, UUID.fromString(userId)) // KEYS[6]
+        );
+
+        // 스크립트를 실행하고 결과를 바로 반환
+        return redisTemplate.execute(cleanupUserLua, keys, userId);
+    }
+}
