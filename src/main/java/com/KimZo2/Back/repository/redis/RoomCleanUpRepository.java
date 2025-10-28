891058657086d1fdb8147e7842bcef2655ab4e5d
조원희 <woni0517@naver.com>
2025-10-28 22:42:50 +0900

    feat: 방 세션 만료시 연결되어 있는 모든 Key 삭제 로직 추가

--- src/main/java/com/KimZo2/Back/repository/redis/RoomCleanUpRepository.java
+++ src/main/java/com/KimZo2/Back/repository/redis/RoomCleanUpRepository.java
@@ -3,6 +3,6 @@ package com.KimZo2.Back.repository.redis;
 import java.util.UUID;
 
 public interface RoomCleanUpRepository {
-
     Long cleanupExpiredUser(UUID roomId, String userId);
+    void deleteAllRoomData(UUID roomId);
 }
--- src/main/java/com/KimZo2/Back/repository/redis/RoomCleanUpRepositoryImpl.java
+++ src/main/java/com/KimZo2/Back/repository/redis/RoomCleanUpRepositoryImpl.java
@@ -1,13 +1,10 @@
 package com.KimZo2.Back.repository.redis;
 
-// RoomCleanupRepositoryImpl.java
-
 import com.KimZo2.Back.util.KeyFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +14,8 @@ public class RoomCleanUpRepositoryImpl implements RoomCleanUpRepository {
 
     private final StringRedisTemplate redisTemplate;
     private final DefaultRedisScript<Long> cleanupUserLua;
+    private final DefaultRedisScript<Long> deleteRoomLua; // 스크립트 주입
+
 
     @Override
     public Long cleanupExpiredUser(UUID roomId, String userId) {
@@ -32,5 +31,21 @@ public class RoomCleanUpRepositoryImpl implements RoomCleanUpRepository {
 
         // 스크립트를 실행하고 결과를 바로 반환
         return redisTemplate.execute(cleanupUserLua, keys, userId);
+    }
+
+    @Override
+    public void deleteAllRoomData(UUID roomId) {
+
+        List<String> keys = List.of(
+                KeyFactory.roomMeta(roomId),       // KEYS[1]
+                KeyFactory.roomMembers(roomId),    // KEYS[2]
+                KeyFactory.roomPos(roomId),        // KEYS[3]
+                KeyFactory.roomSeen(roomId),       // KEYS[4]
+                KeyFactory.roomActive(),           // KEYS[5]
+                KeyFactory.roomHot(),              // KEYS[6]
+                KeyFactory.roomPublic()            // KEYS[7]
+        );
+
+        redisTemplate.execute(deleteRoomLua, keys, roomId.toString());
     }
 }
